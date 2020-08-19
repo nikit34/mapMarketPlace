67c4aa30fed19fab0a7cc858db4717371eb4be7e
nikit34 <permikov134@mail.ru>
2020-08-19 09:37:04 +0500

    adapted CBV for main

--- mapMarketPlace/mapMarketPlace/views.py
+++ mapMarketPlace/mapMarketPlace/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.views import View
+from django.views.generic import TemplateView
 from django.shortcuts import render, redirect
 from django.views.decorators.csrf import ensure_csrf_cookie
 from django.utils.decorators import method_decorator
@@ -9,21 +9,35 @@ from .forms import MarketImageForm
 from .models import MarketImage
 
 
-class authMainView(View):
+class authMainView(TemplateView):
+    template_name = 'base.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(authMainView, self).get_context_data(*args, **kwargs)
+        if self.request.user.is_authenticated:
+            context['is_auth'] = True
+        else:
+            context['is_auth'] = False
+        return context
+
     @method_decorator(ensure_csrf_cookie)
     def get(self, request, *args, **kwargs):
-        # cards = MarketImage.objects.all()
-        return render(request, 'base.html')
+        context = super(authMainView, self).get_context_data(*args, **kwargs)
+        cards = MarketImage.objects.all()
+        context['cards'] = cards
+        return super(authMainView, self).get(request, *args, **kwargs)
 
     @login_required
     def post(self, request, *args, **kwargs):
+        context = super(authMainView, self).get_context_data(*args, **kwargs)
         if not request.user.is_authenticated:
             redirect('login')
         form = MarketImageForm(request.POST or None, request.FILES or None)  # TODO: add instance by ads
         if form.is_valid():
             form.author = request.user
             form.save(commit=True)
-        return render(request, 'base.html', {'form': form})
+        context['form'] = form
+        return super(authMainView, self).post(request, *args, **kwargs)
 
 
 # class guestMainView(LoginRequiredMixin, View):
