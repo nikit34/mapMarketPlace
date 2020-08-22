8cae2eaabf1edb7fb029090e7a917ec7afd51f5a
nikit34 <permikov134@mail.ru>
2020-08-22 12:05:42 +0500

    big fixes

--- mapMarketPlace/mapMarketPlace/filters.py
+++ mapMarketPlace/mapMarketPlace/filters.py
@@ -7,22 +7,11 @@ class MarketImageFilter(django_filters.FilterSet):
     description = django_filters.CharFilter(lookup_expr='icontains')
     card_id = django_filters.CharFilter(lookup_expr='iexact')
 
-    data_publish = django_filters.NumberFilter(field_name='data_publish', lookup_expr='mounth')
-    data_publish__gt = django_filters.NumberFilter(field_name='data_publish', lookup_expr='mounth__gt')
-    data_publish__lt = django_filters.NumberFilter(field_name='data_publish', lookup_expr='mounth__lt')
-
-    timer = django_filters.NumberFilter(field_name='timer', lookup_expr='mounth')
-    timer__gt = django_filters.NumberFilter(field_name='timer', lookup_expr='mounth__gt')
-    timer__lt = django_filters.NumberFilter(field_name='timer', lookup_expr='mounth__lt')
-
-
     class Meta:
         model = MarketImage
         fields = {
             'author',
             'title',
             'description',
-            'data_publish',
-            'timer',
             'card_id',
         }
